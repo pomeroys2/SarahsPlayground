076fd67c986f3be73322c40577cd47409fbd8748
Anthony Wash <anthonywash@ADW002325.fios-router.home>
2019-04-03 22:18:52 -0400

    Added some calculations

--- SarahsPlayground.js
+++ SarahsPlayground.js
@@ -6,3 +6,10 @@ console.log(b);
 
 var c = "I like turtles";
 console.log(c);
+
+var myDecimal = 9.9;
+
+// remainder
+var remainder = 11 % 3; // 2
+console.log("remainder of 11 divided by 3");
+console.log(remainder);
